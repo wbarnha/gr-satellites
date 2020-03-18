410f53d71e6ac32dd5fb997aa146fddcf30f100a
William Barnhart <williambbarnhart@gmail.com>
2020-03-18 11:26:01 -0400

    Converted flowgraphs to hier blocks

--- compile.sh
+++ compile.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 grcc apps/hierarchical/ccsds_descrambler.grc
+grcc apps/ao73.grc
+grcc apps/itasat1.grc
+grcc apps/floripasat_1.grc
+grcc apps/sat_3cat_2.grc
+grcc apps/nayif1.grc
+grcc apps/jy1sat.grc
+grcc apps/generic_1k2_afsk_ax25.grc
 
 for file in apps/hierarchical/*.grc
-do grcc $file
-done
-
-for file in apps/*.grc
 do grcc $file
 done
